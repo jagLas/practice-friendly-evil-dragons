5fe370c29cc931c443ba6cbc40fd85d0cced3f12
jagLas <jglasher@gmail.com>
2022-12-06 11:28:28 -0500

    finish phase 1

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,20 @@
-// Your code here
+class Dragon {
+  constructor (name, color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
+
+  static getDragons(...dragons){
+    let names = [];
+    dragons.forEach(dragon => names.push(dragon.name));
+
+    return names;
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
